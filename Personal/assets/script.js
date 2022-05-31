6f8ce4169384c85fa7b22bf5ae67793f09e8b79b
Aintworth <rakuichijou9@gmail.com>
2022-06-01 03:03:47 +0700

    update submission

--- Personal/assets/script.js
+++ Personal/assets/script.js
@@ -14,6 +14,6 @@ function reOrder(size) {
     }
 }
 const article = document.querySelector("#switch article")
-var size = window.matchMedia("(max-width: 1000px)")
-reOrder(size) // Call listener function at run time
-size.addListener(reOrder) // Attach listener function on state changes 
+var size = window.matchMedia("(max-width: 1080px)")
+reOrder(size)
+size.addListener(reOrder)
